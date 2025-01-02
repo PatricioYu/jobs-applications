effbf66001c28f18ff5d094507c13560c9bb8955
Patricio Yu <yupatricio0@gmail.com>
2025-01-02 18:18:29 -0300

    form structure

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `<h1>Hello world!</h1>`,
-
+  imports: [],
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'applied';
 }
