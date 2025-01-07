009216ac5c7a634a51a9d71a6a1a13d6f5d648e7
Patricio Yu <yupatricio0@gmail.com>
2025-01-07 19:12:20 -0300

    modified form's action attribute value

---
+++ server/api/src/main/java/com/applied/api/service/Service.java
@@ -0,0 +1,8 @@
+package com.applied.api.service;
+
+public class Service implements IService {
+    @Override
+    public void addJobApplication() {
+        // writes the jobApplication into an excel file
+    }
+}
