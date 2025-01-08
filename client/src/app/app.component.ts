d4e73a583836218628934d97575ff112a4f3fdbb
Patricio Yu <yupatricio0@gmail.com>
2025-01-08 19:07:09 -0300

    added form validation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  jobApplicationForm: FormGroup = new FormGroup({
+    date: new FormControl(new Date().toLocaleDateString()),
+    companyName: new FormControl("", [Validators.required]),
+    jobPosition: new FormControl("", [Validators.required])
+  })
 
+  onSubmit() {
+    const formValue: FormGroup = this.jobApplicationForm.value;
+    console.log(formValue)
+  }
 }
